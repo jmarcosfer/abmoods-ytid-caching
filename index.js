94a5dd2564ec372ee4af842219fe597a6bc86bae
Jorge Marcos <jorge.marcosfer@gmail.com>
2021-11-22 17:31:07 +0100

    get all songs metadata from ES

--- index.js
+++ index.js
@@ -31,20 +31,36 @@ async function setupESBackup () {
     }
 }
 
-async function getAllIDs () {
-    esClient.search({
-        index: 'acbrainz_highlevel'
-    })
-}
+async function getAllSongs () {
+    const resp = await esClient.search({
+        index: 'acbrainz_highlevel',
+        scroll: '1h',
+        body: {
+            query: {
+                match_all: {}
+            }
+        },
+        _source: ["_id", "metadata.tags.title", "metadata.tags.artist", "metadata.tags.album"]
+    });
 
-async function getSongDetails (acbrainzID) {
-
+    console.info(`Returned ${resp.body.hits.total.relation} ${resp.body.hits.total.value} results`);
+    return resp.body.hits.hits.map( (h) => {
+        return {
+            mbid: h["_id"],
+            title: h["_source"].metadata.tags.title,
+            artist: h["_source"].metadata.tags.artist,
+            album: h["_source"].metadata.tags.album
+        }
+    });
 }
 
 async function downloadYoutubeID (songDetails) {
     // use 'scraper.youtube(q, key, pageToken)'
 }
 
+async function writeToFile (idMap) {
+    // write mbid -> ytid map to .csv
+}
 
 async function main () {
     try {
@@ -53,6 +69,15 @@ async function main () {
     } catch (err) {
         console.error(`Elasticsearch backup couldn't be restored \n`);
         console.error(err);
+    }
+
+    // 1. Retrieve all songs: mbid + metadata(title, artist, album)
+    const songs = await getAllSongs();
+    const testArray = songs.slice(2);
+
+    // 2. Iterate. For each song: a) search youtube and parse for yt ID; b) save to file; c) wait for ~2sec 
+    for (let i=0; i < testArray.length; i++) {
+        
     }
 }
 
